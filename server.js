b0e0410e6aa1fe159a7b0079762deae810c0d42c
Rodrigo <rodrigo@webprado.com>
2019-01-27 08:28:52 -0200

    Alterado server

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express'),
-    fs = require('fs'),
-    app = express();
-//    eps = require('ejs'),
-//    morgan = require('morgan');
+    app     = express(),
+    morgan  = require('morgan');
+	
+Object.assign=require('object-assign')
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var app = express();
-
-var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
 
 
 // app is running!
@@ -15,10 +15,12 @@ app.get('/', function(req, res) {
     res.send('Hello from NodeJS  at '+ new Date());
 });
 
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
 
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
 
-app.listen(8080, ip);
-
-
-
-module.exports = app;
+module.exports = app ;
